8d58582b118b41a3bd3acb34d766eebfcb6b7840
Abhinav Tripathi <abhinavtripathi01@gmail.com>
2014-03-30 11:33:36 +0530

    beginning

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_host = "localhost";
-$db_user = "abhimon";
-$db_password = "baba123";
-$db_name = "projectHope";
-mysql_connect("$db_host","$db_user","$db_password") or die ("could not connect to mysql");
-mysql_select_db("$db_name") or die ("no database");              
-?>
