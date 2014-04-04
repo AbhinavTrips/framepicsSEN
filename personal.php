e329d2089753d2711496993175048ce5c4f6b8b5
Abhinav Tripathi <abhinavtripathi01@gmail.com>
2014-04-05 03:38:59 +0530

    Added loggedin check

--- personal.php
+++ personal.php
@@ -2,30 +2,22 @@
 require 'connection.php';
 require 'current_page.php';
 if(loggedin()){
-if(isset($_GET['first'])&&isset($_GET['last'])&&isset($_GET['email'])&&isset($_GET['mobile'])){
-$user = getuserfield['userid'];
-echo $user
-$first = mysql_real_escape_string($_GET['first']);
-$last = mysql_real_escape_string($_GET['last']);
-$email = mysql_real_escape_string($_GET['email']);
-$mobile = mysql_real_escape_string($_GET['mobile']);
-			$update_personal = "UPDATE `users` SET `first_name`='$first',`last_name`='$last',`email_id`='$email',`mobile`='$mobile' WHERE `user_id` = '$user'";
-				if($query_run_update = mysql_query($update_pass)){
-				$query_num_rows = mysql_num_rows($query_run_check);
-					if($query_num_rows==1){
-					echo 'Password Updated successfully';
-					}else{
-					echo 'Could not change password';
-					}
-				} 	
+	if(isset($_GET['first'])||isset($_GET['last'])||isset($_GET['email'])||isset($_GET['mobile'])){
+	$user = getuserfield('user_id');
+	//echo $user;
+	$first = mysql_real_escape_string($_GET['first']);
+	$last = mysql_real_escape_string($_GET['last']);
+	$email = mysql_real_escape_string($_GET['email']);
+	$mobile = mysql_real_escape_string($_GET['mobile']);
+	$update_personal = "UPDATE `users` SET `first_name`='$first',`last_name`='$last',`email_id`='$email',`mobile`='$mobile' WHERE `user_id` = '$user'";
+			if($query_run_update = mysql_query($update_personal)){
+			echo 'Personal Information updated successfully';
 			}else{
-			echo 'Old password does not match our records';
+			echo 'Could not update information';
 			}
-		}
+				
 	}
-	}
-}
-else{
-echo 'Please login to continue';
+}else{
+ echo 'Please login to continue';
 }
 ?>
