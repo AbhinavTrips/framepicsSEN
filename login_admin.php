330f97b384e7eca65df1fecf5da601634bb0dd1a
Abhinav Tripathi <abhinavtripathi01@gmail.com>
2014-04-05 19:07:00 +0530

    login sql included

--- login_admin.php
+++ login_admin.php
@@ -1,3 +1,31 @@
+<?php
+require 'connection.php';
+require 'admin.php';
+//require 'current_page.php';
+if(isset($_POST['login_user'])&&isset($_POST['login_pass'])){
+$user = $_POST['login_user'];
+$password = $_POST['login_pass'];
+echo $user;
+echo $password;
+$query = "SELECT `adminid` FROM `admin` WHERE `name`='".mysql_real_escape_string($user)."' AND `password`='".mysql_real_escape_string($password)."' ";
+if($query_run = mysql_query($query)){
+	$query_num_rows = mysql_num_rows($query_run);
+	if($query_num_rows==0){
+		echo 'user does not exist';
+	} 	else if($query_num_rows==1){
+		$user_id = mysql_result($query_run,0,'adminid');
+		$_SESSION['adminid']= $user_id;
+		//header('Location: home.php');
+		
+
+		}
+	}
+}
+if(adloggedin()){
+	$name = getadminfield('adminid');
+	echo $name;
+	}
+?> 
 
 <!DOCTYPE html>
 <html lang="en">
@@ -31,15 +59,19 @@
 
     <div class="container">
 
-      <form class="form-signin" role="form">
-        <h2 class="form-signin-heading">Admin Login</h2>
-        <input type="email" class="form-control" placeholder="Email address" required autofocus>
-        <input type="password" class="form-control" placeholder="Password" required>
-        <label class="checkbox">
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      </form>
+
+ 
+  <form class="form-signin" role="form" action="<?php echo $current_file; ?>" method="post">
+         <h2 class="form-signin-heading">Please sign in</h2>
+
+            <div class="form-group">
+              <input name ="login_user" type="text" placeholder="Email" class="form-control" required>
+            </div>
+            <div class="form-group">
+              <input name="login_pass" type="password" placeholder="Password" class="form-control" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Sign in</button>
+          </form> 
 
     </div> <!-- /container -->
 
