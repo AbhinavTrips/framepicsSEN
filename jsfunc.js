2febd9cd5f5f514eac2a81b2b13943b8b0388e8e
Abhinav Tripathi <abhinavtripathi01@gmail.com>
2014-04-07 22:34:30 +0530

    added deliveryUpdate and statusUpdate functions along with loading image

--- jsfunc.js
+++ jsfunc.js
@@ -1,5 +1,182 @@
-﻿
+﻿function deliveryUpdate($id){
+ var ajaxRequest;  // The variable that makes Ajax possible!
+	  try{
+   // Opera 8.0+, Firefox, Safari
+   ajaxRequest = new XMLHttpRequest();
+ }catch (e){
+   // Internet Explorer Browsers
+   try{
+      ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+   }catch (e) {
+      try{
+         ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+      }catch (e){
+         // Something went wrong
+         alert("Your browser broke!");
+         return false;
+      }
+   }
+ }
+ // Create a function that will receive data 
+ // sent from the server and will update
+ // div section in the same page.
+ ajaxRequest.onreadystatechange = function(){
+ 
+   if(ajaxRequest.readyState != 4){
+   var a = 'img';
+   var id = $id;
+   var load = a.concat($id);
+ // alert(date);
+ document.getElementById(load).setAttribute('src','images/ajax-loader.gif');
 
+    //  var ajaxDisplay = document.getElementById(date);
+	// ajaxDisplay.innerHTML = ajaxRequest.responseText;
+   }
+
+   if(ajaxRequest.readyState == 4){
+   var dat = 'date';
+   var id = $id;
+   var date = dat.concat($id);
+ // alert(date);
+      var ajaxDisplay = document.getElementById(date);
+	 ajaxDisplay.innerHTML = ajaxRequest.responseText;
+   }
+ }
+ // Now get the value from user and pass it to
+ // server script.
+var del = 'dates';
+var deldate = del.concat($id);
+//alert(deldate);
+var delivery = document.getElementById(deldate).value;
+var oid = $id;
+alert(oid);
+var queryString = "?del=" + delivery ;
+queryString += "&oid=" +oid;
+alert(queryString);
+ajaxRequest.open("GET", "deliveryUpdate.php" + 
+                              queryString, true);
+ ajaxRequest.send(null); 
+}
+
+
+
+
+
+
+
+
+function statusUpdate($id){
+ var ajaxRequest;  // The variable that makes Ajax possible!
+	  try{
+   // Opera 8.0+, Firefox, Safari
+   ajaxRequest = new XMLHttpRequest();
+ }catch (e){
+   // Internet Explorer Browsers
+   try{
+      ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+   }catch (e) {
+      try{
+         ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+      }catch (e){
+         // Something went wrong
+         alert("Your browser broke!");
+         return false;
+      }
+   }
+ }
+ // Create a function that will receive data 
+ // sent from the server and will update
+ // div section in the same page.
+ ajaxRequest.onreadystatechange = function(){
+   if(ajaxRequest.readyState != 4){
+   var b = 'imgs';
+   var id = $id;
+   var loads = b.concat($id);
+ // alert(date);
+ document.getElementById(loads).setAttribute('src','images/ajax-loader.gif');
+
+    //  var ajaxDisplay = document.getElementById(date);
+	// ajaxDisplay.innerHTML = ajaxRequest.responseText;
+   }
+ 
+   if(ajaxRequest.readyState == 4){
+   var stat = 'stat';
+   var id = $id;
+   var stats = stat.concat($id);
+  //alert(stats);
+      var ajaxDisplay = document.getElementById(stats);
+	 ajaxDisplay.innerHTML = ajaxRequest.responseText;
+   }
+ }
+ // Now get the value from user and pass it to
+ // server script.
+var state = 'status';
+var statusq = state.concat($id);
+alert(statusq);
+var status = document.getElementById(statusq).value;
+var oid = $id;
+alert(oid);
+var queryString = "?status=" + status ;
+queryString += "&oid=" +oid;
+alert(queryString);
+ajaxRequest.open("GET", "statusUpdate.php" + 
+                              queryString, true);
+ ajaxRequest.send(null); 
+}
+
+
+
+
+
+function address(){
+ var ajaxRequest;  // The variable that makes Ajax possible!
+	  try{
+   // Opera 8.0+, Firefox, Safari
+   ajaxRequest = new XMLHttpRequest();
+ }catch (e){
+   // Internet Explorer Browsers
+   try{
+      ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+   }catch (e) {
+      try{
+         ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+      }catch (e){
+         // Something went wrong
+         alert("Your browser broke!");
+         return false;
+      }
+   }
+ }
+ // Create a function that will receive data 
+ // sent from the server and will update
+ // div section in the same page.
+ ajaxRequest.onreadystatechange = function(){
+ 
+   if(ajaxRequest.readyState == 4){
+      var ajaxDisplay = document.getElementById('newAdd');
+	 ajaxDisplay.innerHTML = ajaxRequest.responseText;
+   }
+ }
+ // Now get the value from user and pass it to
+ // server script.
+var first = document.getElementById('first').value;
+var last = document.getElementById('last').value;
+var add1 = document.getElementById('add1').value;
+var add2 = document.getElementById('add2').value;
+var pin = document.getElementById('code').value;
+var city = document.getElementById('city').value;
+var state = document.getElementById('state').value;
+var email = document.getElementById('mail').value;
+var mobile = document.getElementById('mobi').value;
+var queryString = "?first=" + first ;
+queryString +=  "&last=" + last + "&add1=" + add1 + "&add2=" + add2 + "&city=" + city + "&state=" + state  + "&pin=" + pin + "&email=" + email + "&mobile=" + mobile;
+alert(queryString);
+ajaxRequest.open("GET", "new_add.php" + 
+                              queryString, true);
+ ajaxRequest.send(null); 
+}
+
+////////////////////////////////////////////////////////////// Function to update password on dashboard.php page //////////////////////////////////////////////////////////////////
 //Browser Support Code
 function updatepass(){
  var ajaxRequest;  // The variable that makes Ajax possible!
@@ -43,7 +220,7 @@ ajaxRequest.open("GET", "pass_update_query.php" +
  ajaxRequest.send(null); 
 }
 
-
+///////////////////////////////////////////////////////// Function to choose a default address on dashboard.php page //////////////////////////////////////////////////////////////
 
 function setDefault(id){
  var ajaxRequest;  // The variable that makes Ajax possible!
@@ -87,7 +264,7 @@ alert(queryString);
  ajaxRequest.send(null); 
 }
 
-
+///////////////////////////////////////////////////////// Function to delete a stored address on dashboard.php page ///////////////////////////////////////////////////////////////
 function add_del(id){
  var id = id;
  var ajaxRequest;  // The variable that makes Ajax possible!
@@ -132,5 +309,50 @@ ajaxRequest.open("GET", "add_del.php" + queryString, true);
 }
 
 
+///////////////////////////////////////////////////////// Function to update personal information on dashboard.php page ///////////////////////////////////////////////////////////
 
+function personal(){
+ var ajaxRequest;  // The variable that makes Ajax possible!
+	  try{
+   // Opera 8.0+, Firefox, Safari
+   ajaxRequest = new XMLHttpRequest();
+ }catch (e){
+   // Internet Explorer Browsers
+   try{
+      ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+   }catch (e) {
+      try{
+         ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+      }catch (e){
+         // Something went wrong
+         alert("Your browser broke!");
+         return false;
+      }
+   }
+ }
+ // Create a function that will receive data 
+ // sent from the server and will update
+ // div section in the same page.
+ ajaxRequest.onreadystatechange = function(){
+ 
+   if(ajaxRequest.readyState == 4){
+      var ajaxDisplay = document.getElementById('pers');
+	 ajaxDisplay.innerHTML = ajaxRequest.responseText;
+   }
+ }
+ // Now get the value from user and pass it to
+ // server script.
+var firstname = document.getElementById('firstName').value;
+var lastname = document.getElementById('lastName').value;
+var email = document.getElementById('email').value;
+var mobile = document.getElementById('mobile').value;
+var queryString = "?first=" + firstname ;
+queryString +=  "&last=" + lastname + "&email=" + email + "&mobile=" + mobile;
+alert(queryString);
+ajaxRequest.open("GET", "personal.php" + 
+                              queryString, true);
+ ajaxRequest.send(null); 
+}
+
+///////////////////////////////////////////////////////// Function to update personal information on dashboard.php page ///////////////////////////////////////////////////////////
 
