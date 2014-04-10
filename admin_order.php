94bb6477130c83681d67959bb7a944ef56e14953
Abhinav Tripathi <abhinavtripathi01@gmail.com>
2014-04-11 00:22:11 +0530

    SQL to display all order table

--- admin_order.php
+++ admin_order.php
@@ -1,6 +1,6 @@
 <?php
 require 'connection.php';
-require 'admin.php';
+//require 'admin.php';
 if(adloggedin()){
 /*$user = $_SESSION['user_id'];
 */$query = "SELECT * FROM `order_frame` WHERE '1'";
@@ -54,7 +54,7 @@ if($run = mysql_query($query)){
                   <div class="row">
                   <div class="col-md-12">
     			  <div class="input-group">
-     			  <input type="text" class="form-control" id ="status'.$oid.'" name ="status" type="text" placeholder="Status">
+     			  <input type="text" class="form-control" id ="status'.$oid.'" name ="status" type="text" placeholder="'.$status.'">
      			  <span class="input-group-btn">
      			  <button id = "'.$oid.'" class="btn btn-default" type="button" onclick="statusUpdate('.$oid.')"><span class="glyphicon glyphicon-ok"></span></button>
      			  </span></div>
@@ -69,13 +69,15 @@ if($run = mysql_query($query)){
                   <div class="row">                    
 	     	         <div class="col-md-6">
 	            		   <div class="input-group">
-	     	      		   <input id ="dates'.$oid.'" name ="status" type="date" placeholder="'.$delivery.'" class="form-control" value="2014-12-31">
+	     	      		   <input id ="dates'.$oid.'" name ="status" type="date" placeholder="'.$delivery.'" class="form-control" value="'.$delivery.'">
    	        	    	   <span class="input-group-btn">    	         
 					 	   <button id = "dateb'.$oid.'" type="button" class="btn btn-primary" onclick="deliveryUpdate('.$oid.')"><span class="glyphicon glyphicon-ok"></span></button></span></div>	     	        
 				  </div>
 				  </div>
 			         <div class = "row ">
-			         <div class = "col-md-10" id = "date'.$oid.'"><img align = "center" id="img'.$oid.'"></div>
+			         <div class = "col-md-10" id = "date'.$oid.'">
+			         <label id="label'.$oid.'"></label>
+			         <img align = "center" id="img'.$oid.'"></div>
 			         <div class = "row "> </div>
 
 			         </div>
