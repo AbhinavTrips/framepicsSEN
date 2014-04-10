22594db74a2325754e46dc61d088f433e1bf361b
Abhinav Tripathi <abhinavtripathi01@gmail.com>
2014-04-11 03:28:41 +0530

    Access restiction to only user logged in as admin

--- home_upload.php
+++ home_upload.php
@@ -1,6 +1,7 @@
 <?php
 require 'connection.php';
-//require 'current_page.php';
+require 'admin.php';
+if(adloggedin()){
 $structure = 'images/frame_demo';
 $filename = $structure;
 if(isset($_FILES['choose_file1'])){
@@ -26,25 +27,13 @@ if(isset($name1)){
 	}
 }
 }
-?>
 
-<!--<!DOCTYPE html>-->
-<!--<html>
-
-<head>
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
-<title>Untitled 1</title>
-<link href="../framePics/css/bootstrap.min.css" rel="stylesheet">
-<!-- Custom styles for this template -->
-<!--<link href="../framePics/dashboard.css" rel="stylesheet">
-</head>
-
-<body>
---><form action="home_upload.php" method ="post" enctype="multipart/form-data">
+echo'
+<form action="home_upload.php" method ="post" enctype="multipart/form-data">
 <input type="file" name="choose_file1" id="choose_file1" class="form-control" placeholder="corners file"><br><br>
 <input type="submit" name="upload">
-</form>
-<!--</body>
-
-</html>
--->
+</form>';
+}
+else{
+echo 'Please login to upload files';
+}
