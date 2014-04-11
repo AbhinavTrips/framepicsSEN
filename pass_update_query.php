ebdca61ac9e934fec74b55c93b60fd034e6f92b7
Abhinav Tripathi <abhinavtripathi01@gmail.com>
2014-04-11 21:35:45 +0530

    sql now ensures that password update fields are not blank

--- pass_update_query.php
+++ pass_update_query.php
@@ -2,13 +2,14 @@
 require 'connection.php';
 require 'current_page.php';
 if(loggedin()){
-if(isset($_GET['oldpass'])&&isset($_GET['newpass'])&&isset($_GET['repass'])){
+$user = getuserfield('user_id');
+if(isset($_GET['oldpass'])&&isset($_GET['newpass'])&&isset($_GET['repass'])&&!empty($_GET['oldpass'])&&!empty($_GET['newpass'])&&!empty($_GET['repass'])){
 
 $oldpass = mysql_real_escape_string($_GET['oldpass']);
 $newpass = mysql_real_escape_string($_GET['newpass']);
 $repass = mysql_real_escape_string($_GET['repass']);
 	if($newpass == $repass){
-	$passcheck = "SELECT `password` FROM `users` WHERE `user_id` = '1' AND `password`='$oldpass'";
+	$passcheck = "SELECT `password` FROM `users` WHERE `user_id` = '$user' AND `password`='$oldpass'";
 		if($query_run_check = mysql_query($passcheck)){
 		$query_num_rows = mysql_num_rows($query_run_check);
 			if($query_num_rows==1){
@@ -28,6 +29,8 @@ $repass = mysql_real_escape_string($_GET['repass']);
 	}else{
 	 echo 'Passwords do not match';
 	}
+	}else{
+	echo 'All fields are mandatory';
 	}
 }
 else{
