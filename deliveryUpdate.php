53e888c505449628b9f14b6ba282df3c897590d8
Abhinav Tripathi <abhinavtripathi01@gmail.com>
2014-04-26 17:59:40 +0530

    final comments

--- deliveryUpdate.php
+++ deliveryUpdate.php
@@ -1,12 +1,17 @@
 <?php
 require 'connection.php';
 require 'admin.php';
+
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+ 													SQL to update delivery status. Implemented by Abhinav Tripathi
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
 if(isset($_GET['del'])&&!empty($_GET['del'])){
 $date = mysql_real_escape_string($_GET['del']);
 $oid = mysql_real_escape_string($_GET['oid']);
-//echo 'delivery : '.$date;
-//echo 'Order : '.$oid;
-$query = "UPDATE `order_frame` SET `delivery`='$date' WHERE `orderid` ='$oid'";
+$query = "UPDATE `order_frame` SET `delivery`='$date' WHERE `orderid` ='$oid'";//update delivery status in order_frame table
 if($query_run = mysql_query($query)){
 echo 'Successfully updated delivery status';
 }else{
