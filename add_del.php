b455c23a373226c5edc0b07c741c34ac2f430118
Abhinav Tripathi <abhinavtripathi01@gmail.com>
2014-04-26 12:43:25 +0530

    updated query, added description

--- add_del.php
+++ add_del.php
@@ -1,8 +1,13 @@
 <?php
+/**********************************************************************************************************************************************************************************
+
+							Implemented by Abhinav Tripathi. Deletes an address stored by the user. Called from add_del function in jsfunc.js file
+
+**********************************************************************************************************************************************************************************/
 require 'connection.php';
 require 'current_page.php';
 $aid = mysql_real_escape_string($_GET['id']);
 $query = "DELETE FROM `address` WHERE `aid` ='$aid'";
 mysql_query($query);
-echo $aid;
+echo 'Removed address, reload to see changes';
 ?>
