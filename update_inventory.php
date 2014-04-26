7ed123b92446132692a34b4cecfed7062c3d6662
Abhinav Tripathi <abhinavtripathi01@gmail.com>
2014-04-26 18:18:06 +0530

    Final comments

--- update_inventory.php
+++ update_inventory.php
@@ -1,9 +1,15 @@
 <?php
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+                                                SQL for updating inventory. Implemented by Abhinav Tripathi 
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
 require 'connection.php';
 $stock = $_POST['stock'];
 $base = $_POST['base'];
 $fname = $_POST['fname'];
-$query = "UPDATE `frames` SET `stock`='$stock',`baseprice`='$base' WHERE `fname`='$fname'";
+$query = "UPDATE `frames` SET `stock`='$stock',`baseprice`='$base' WHERE `fname`='$fname'";// Update details in frames table
 if($query_run = mysql_query($query)){
 echo 'update successful';
 header('Location:dashboard_admin.php');
