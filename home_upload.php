2862145455c54c506e6038338d52bc0e648edb04
Abhinav Tripathi <abhinavtripathi01@gmail.com>
2014-04-26 15:36:01 +0530

    Section wise description

--- home_upload.php
+++ home_upload.php
@@ -1,8 +1,12 @@
 <?php
 require 'connection.php';
-require 'admin.php';
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+ 									Form to let the admin add new images to the gallery on the home page. Implemented by Abhinav Tripathi.
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 if(adloggedin()){
-$structure = 'images/frame_demo';
+$structure = 'images/frame_demo';//images for gallery are stored in "images/frame_demo" folder
 $filename = $structure;
 if(isset($_FILES['choose_file1'])){
 $name1 = $_FILES['choose_file1']['name'];
@@ -14,25 +18,25 @@ if(isset($name1)){
 	echo 'OK'.'<br>';
 	$location_pic = $structure.'/';
 	echo $location_pic;
-	//$name1 = 'corners.png';
+	// Upload routine
 		if(($ext1=='png'|| $ext1=='jpeg'|| $ext1=='jpg') && move_uploaded_file($tmp1,$location_pic.$name1)){
 		echo 'successfully uploaded1';
-		//echo $user;
 		}else{
 		echo 'Error occurred while uploading11 '.$name1.' </br>Please choose a jpeg or png file';
-		//include 'upload_doc.php';
 		}
 	}else{
 	echo 'No File was chosen1';
 	}
 }
 }
-
+//form for uploading
 echo'
+
 <form action="home_upload.php" method ="post" enctype="multipart/form-data">
 <input type="file" name="choose_file1" id="choose_file1" class="form-control" placeholder="corners file"><br><br>
 <input type="submit" name="upload">
-</form>';
+</form><br>
+';
 }
 else{
 echo 'Please login to upload files';
