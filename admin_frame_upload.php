c6645af069ac0b229d6ec08c7f690c9580ddf242
Abhinav Tripathi <abhinavtripathi01@gmail.com>
2014-04-26 14:48:04 +0530

    Added description for various modules added

--- admin_frame_upload.php
+++ admin_frame_upload.php
@@ -4,6 +4,11 @@ require 'admin.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+<!---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+ 					Page to let the admin upload new frames. Form implemented in upload_adfile.php which is included separately. Implemented by Abhinav Tripathi
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -31,7 +36,7 @@ require 'admin.php';
   </head>
 
   <body>
-
+<!------------------------------------------------------------------- Navigation Bar starts here --------------------------------------------------------------------------------->
 <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
@@ -46,22 +51,25 @@ require 'admin.php';
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
         <li class="active"><a href="#"><span class="glyphicon glyphicon-home"></span></a></li>
-<!--            <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
--->        <?php    
-            if(adloggedin()){
+        <?php    
+            if(adloggedin()){// if admin is logged in, show logout option else show login option
 		echo '<li><a href="logout.php"><span class="glyphicon glyphicon-off"></span> Logout</a></li>';}
 			else{
 		echo '<li><a href="login_admin.php"><span class="glyphicon glyphicon-off"></span> Login</a></li>';
 			}
 			?>
-<!--			 <li><a href="dashboard_admin.php"><span class="glyphicon glyphicon-user"></span></a></li>            
--->            </ul>
+           </ul>
 		 </div>
       </div>
     </div>
+<!-------------------------------------------------------------------- Navigation Bar ends here ---------------------------------------------------------------------------------->
+
 
     <div class="container-fluid">
       <div class="row">
+      
+<!----------------------------------------------------------------------- Side Bar starts here ----------------------------------------------------------------------------------->
+      
         <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
             <li><a href="dashboard_admin.php">Pending Orders</a></li>
@@ -70,9 +78,18 @@ require 'admin.php';
             <li><a href="#inventory">Manage Inventory</a></li>
             <li><a href="admin_inventory.php">Inventory</a></li>
           </ul>
-      
         </div>
+        
+<!----------------------------------------------------------------------- Side Bar ends here ------------------------------------------------------------------------------------->
+        
 <?php
+if(adloggedin()){
+/*
+
+File upload module included here in the file 'upload_adfile.php'. Admin can upload new frame pictures and details.
+
+*/
+
 echo     '<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <div class="row">
           	<h2>Upload New Frame</h2>
@@ -81,14 +98,47 @@ echo     '<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
 echo'
  			  <div class="row" id="updatePer">
 			  </div>
-			</div>
-			<div class="col-md-4 col-md-offset-1"><br>
-				<div class="row" id="inventory">
+			</div>';
+			
+/*
+
+Inventory Management module included here in the file 'ad_inventory.php'. Admin can change the base price and stock of the existing items.
+
+*/
+
+echo	'<div class="col-md-4 col-md-offset-1"><br>
+			 <div class="row" id="inventory">
 				<h2  id="addresses">Manage Inventory</h2>';
 				include 'ad_inventory.php';
 echo'
+		     </div>
+		 </div>';
+
+/*
+
+Home Gallery Image module included here in the file 'home_upload.php'. Admin can upload a picture here, it gets displayed on the home gallery.
+
+*/
+		 
+echo'	<div class="col-md-4 col-md-offset-1"><br>
+			<div class="row" id="inventory">
+				<h2  id="addresses">Add image to home gallery</h2>';
+				include 'home_upload.php';
+echo'
 			</div>
 		</div>';
+		}
+
+/*
+
+Home Gallery Image module included here in the file 'home_upload.php'. Admin can upload a picture here, it gets displayed on the home gallery.
+
+*/
+
+		else{
+		echo 'Please login as administrator to continue';
+		header('Location:login_admin.php');
+		}
 		?>
 
           </div>
